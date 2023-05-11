48f6998c239080141d7eb92cdcfc60c483f999a9
StreamPay <streamprotocol@protonmail.com>
2023-05-11 11:53:23 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -710,7 +710,7 @@ export default function Home() {
                       onClick={() => setModalOpen(true)}
                       className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
                     >
-                      Buy now with Solana Pay
+                      Buy now with StreamPay
                     </button>
                   </div>
                   <div className="mt-6 text-center">
